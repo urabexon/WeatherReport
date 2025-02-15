f04928e419b90a43f94634ba683bb412cc98f417
urabexon <hiroki708.a48uk@gmail.com>
2025-02-15 18:22:37 +0900

    info.plist設定

--- WeatherAndMap/Views/ContentView.swift
+++ WeatherAndMap/Views/ContentView.swift
@@ -8,6 +8,13 @@
 import SwiftUI
 
 struct ContentView: View {
+    // APIへリクエスト、レスポンスの値を保持するオブジェクト
+    @StateObject private var weatherVM = WeatherViewModel()
+    
+    // 緯度・経度
+    var lat: Double = 39.91167
+    var lon: Double = 141.093459
+    
     var body: some View {
         VStack {
             Image(systemName: "globe")
@@ -16,6 +23,9 @@ struct ContentView: View {
             Text("Hello, world!")
         }
         .padding()
+        .onAppear {
+            weatherVM.request3DaysForecast(lat: lat, lon: lon)
+        }
     }
 }
 
