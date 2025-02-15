c45ef91cac3dce14152ce73e11282294af4485bc
urabexon <hiroki708.a48uk@gmail.com>
2025-02-15 18:14:28 +0900

    ViewModel設定

--- WeatherAndMap/WeatherViewModel.swift
+++ WeatherAndMap/WeatherViewModel.swift
@@ -4,5 +4,43 @@ import Alamofire
 
 // APIレスポンスを保持するクラス
 class WeatherViewModel: ObservableObject {
+    // リクエスト用URLの共通部分とAPIキー
+    let baseUrl = APIProperties().baseUrl
+    let apiKey = APIProperties().myAPIKey
     
+    // レスポンスの値を保持する変数
+    @Published var forecast: Forecast?
+    @Published var location: UserLocation?
+    
+    // 3日間予報リクエスト、引数は latが緯度、lonが経度
+    func request3DaysForecast(lat: Double, lon: Double) {
+        // リクエストするためのURLを生成
+        let url = URL(string: baseUrl + "/forecast.json")!
+        // リクエストに含めるパラメータを用意
+        let parameters: Parameters = [
+            "key": apiKey,        // APIキー
+            "q": "\(lat),\(lon)", // 緯度、経度
+            "days": "3",          // 予報を取得する日数、今回は3日分
+            "lang": "ja"          // Conditionのtextの言語、日本語を指定
+        ]
+        
+        // 実際のリクエスト→レスポンス部分
+        AF.request(url, method: .get, parameters: parameters)
+            .responseDecodable(of: WeatherInfo.self) { response in
+                switch response.result {
+                // 通信成功
+                case .success(let data):
+                    // レスポンスの値を保持する変数に渡す
+                    self.forecast = data.forecast
+                    self.location = data.location
+                    
+                    // ロケーション確認用
+                    print("LOCATION:", data.location.name)
+                // 通信失敗
+                case .failure(let err):
+                    print("FAILED:", err)
+                }
+
+            }
+    }
 }
