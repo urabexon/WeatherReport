e4470aafff716b3bc1fc7151cd98475bab4992d2
urabexon <hiroki708.a48uk@gmail.com>
2025-02-15 16:31:14 +0900

    Initial Commit

---
+++ WeatherAndMap/WeatherAndMapApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAndMapApp.swift
+//  WeatherAndMap
+//
+//  Created by 卜部大輝 on 2025/02/15.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAndMapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ WeatherAndMapTests/WeatherAndMapTests.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAndMapTests.swift
+//  WeatherAndMapTests
+//
+//  Created by 卜部大輝 on 2025/02/15.
+//
+
+import Testing
+@testable import WeatherAndMap
+
+struct WeatherAndMapTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
