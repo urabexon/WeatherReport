f32b421a92bf35b781213ef4c5224390555b46ad
urabexon <hiroki708.a48uk@gmail.com>
2025-02-16 10:09:38 +0900

    add section

--- WeatherAndMap/Views/DailyWeatherView.swift
+++ WeatherAndMap/Views/DailyWeatherView.swift
@@ -47,6 +47,44 @@ struct DailyWeatherView: View {
                     .clipShape(.rect(cornerRadius: 10))
                 }
             }
+            // データが無いとき(または起動直後)に表示
+            HStack {
+                ForEach(0...2, id: \.self) { _ in
+                    // MARK: - 1日分の天気予報のUI
+                    VStack(spacing: 5) {
+                        // 日付(年月日)
+                        Text("____年__月__日")
+                        
+                        // 天気アイコン
+                        Image(systemName: "cloud.sun")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 64, height: 64)
+                        
+                        // 天気の説明(晴れ、曇りなど)
+                        Text("晴れのち曇り")
+                        
+                        // 最高気温 / 最低気温
+                        HStack {
+                            Text("最高") // 数字が入る
+                            Text("℃ /")
+                            Text("最低") // 数字が入る
+                            Text("℃")
+                        }
+                        
+                        // 洪水確率 〇〇 %
+                        HStack {
+                            Text("降水確率:")
+                            Text("〇〇") // 数字が入る
+                            Text("%")
+                        }
+                    }
+                    .padding()
+                    .frame(width: ScreenInfo.width / 2, height: ScreenInfo.height / 3)
+                    .background(.yellow.opacity(0.3))
+                    .clipShape(.rect(cornerRadius: 10))
+                }
+            }
         }
     }
 }
